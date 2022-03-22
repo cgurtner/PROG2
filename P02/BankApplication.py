8ae6cac7d554cf07fd81797265d23e9f717b40af
Cyrill Gurtner <me@cgurtner.ch>
2022-03-23 00:24:46 +0100

    refactor BankApplication

--- P02/BankApplication.py
+++ P02/BankApplication.py
@@ -18,28 +18,27 @@ class BankApplication:
     def has_accounts(self) -> bool:
         return len(self.get_accounts()) > 0
 
-    def add_account(self, index, account: BankAccount) -> None:
-        self.accounts[index] = account
-
-    def list_accounts(self) -> None:
-        for key, obj in self.get_accounts().items():
-            print(obj)
+    def add_account(self, account: BankAccount) -> None:
+        self.accounts[account.get_iban()] = account
     
     def run(self):
         while True:
             if not self.client:
                 print('###### Bank of Pythonia ###### ')
                 print('You are not a client yet. To access your E-Banking menu please give us some information:')
-                first_name = input('Your first name: ')
-                name = input('Your last name: ')
-                address = input('Please give us your address: ')
-                birthday = input('Your date of birth in the format YYYY-MM-DD: ')
-                self.client = Client(first_name, name, address, birthday)
+                self.client = self.open_client()
             if not self.has_accounts():
                 print('You have no accounts yet, please open one:')
-                self.open_account()
-                
-    def open_account(self) -> None:
+                self.add_account(self.open_account())
+
+    def open_client(self) -> Client:
+        first_name = input('Your first name: ')
+        name = input('Your last name: ')
+        address = input('Please give us your address: ')
+        birthday = input('Your date of birth in the format YYYY-MM-DD: ')
+        return Client(first_name, name, address, birthday)
+
+    def open_account(self) -> BankAccount:
         created_account = False
         # this explizit bool()-cast is needed because it trigger the __bool__ magic inside YouthAccount
         while bool(created_account) == False:
@@ -48,7 +47,7 @@ class BankApplication:
                 created_account = SavingAccount(self.get_client())
             elif choice == 2:
                 created_account = YouthAccount(self.get_client())
-        self.add_account(created_account.get_iban(), created_account)
+        return created_account
 
 if __name__ == '__main__':
     app = BankApplication()
