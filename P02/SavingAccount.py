5d1fea47a0eb76b7969836e9e9c752030fe0f55f
Cyrill Gurtner <me@cgurtner.ch>
2022-03-22 21:13:22 +0100

    __init__(), set_monthtly_interest(), withdraw() in SavingAccount

--- P02/SavingAccount.py
+++ P02/SavingAccount.py
@@ -1,13 +1,19 @@
 from BankAccount import BankAccount
 
 class SavingAccount(BankAccount):
+    WITHDRAW_COMMISSION = 0.02
+
     def __init__(self, is_open=True, balance=0, currency='CHF', monthly_interest=0.001):
         super().__init__(is_open, balance, currency)
         self.monthly_interest = monthly_interest
 
+    def set_monthly_interest(self, mi) -> None:
+        self.monthly_interest = mi
+
+    # Overrides super().withdraw()
     def withdraw(self, amount) -> None:
         if not self.is_open:
             print('this account is closed')
         elif self.balance - amount < 0:
-            amount += amount * 0.02
+            amount += amount * SavingAccount.WITHDRAW_COMMISSION
         self.balance -= amount
