f6d68fc11ae68ab99f1d68483a52cdeaac80cb9a
Cyrill Gurtner <me@cgurtner.ch>
2022-03-25 19:20:44 +0100

    various cleanups

--- P02/Simulation.py
+++ P02/Simulation.py
@@ -4,33 +4,47 @@ from Client import Client
 from time import time
 
 clientA = Client('Cyrill', 'Gurtner', 'Bahnhofstrasse 40', '1992-07-20')
+print('====== TEST saA ======')
 saA = SavingAccount(clientA)
 saA.deposit(2000)
 saA.deposit(200)
+print(saA)
 
 print()
+print('====== TEST saAA ======')
+saAA = SavingAccount(clientA)
+saAA.deposit(2000)
+saAA.deposit(200)
+# this should withdraw 3060 because of 2% commission on too high withdraw
+# balance should be -860 CHF
+saAA.withdraw(3000)
+print(saAA)
+
+print()
+print('====== TEST yaA ======')
 clientB = Client('A', 'B', 'Bahnhofstrasse 40', '1992-07-20')
-yaB = YouthAccount(clientB) 
+yaA = YouthAccount(clientB) 
 # error message because account is immediatly closed on creation because of date of birth
-yaB.deposit(200) 
+yaA.deposit(200) 
 
 print()
+print('====== TEST yaB ======')
 clientC = Client('A', 'B', 'Bahnhofstrasse 40', '2000-07-20')
-yaC = YouthAccount(clientC)
-yaC.deposit(2000)
-yaC.deposit(200)
-yaC.withdraw(200)
-yaC.withdraw(400)
-yaC.withdraw(1000)
+yaB = YouthAccount(clientC)
+yaB.deposit(2000)
+yaB.deposit(200)
+yaB.withdraw(200)
+yaB.withdraw(400)
+yaB.withdraw(1000)
 # this should throw a message, because it would withdraw more than 2k in same month
 # balance should remain 600.-
-yaC.withdraw(500) 
-print(yaC)
+yaB.withdraw(500) 
+print(yaB)
 
 print()
 print('We start the month-loop with:')
 print(saA)
-print(yaC)
+print(yaB)
 print()
 
 # simulate three months
@@ -40,9 +54,9 @@ while t_end >= time():
     if abs(t_start - time()) % 10 == 0 or t_end - time() == 0:
         print('Monthly interest has been payed!')
         saA.pay_monthly_interest()
-        yaC.pay_monthly_interest()
+        yaB.pay_monthly_interest()
         print()
 
 print('After three months following changed:')
 print(saA)
-print(yaC)
+print(yaB)
