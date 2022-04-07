d3e8f591caa0cac5c943d5485d61123915395ab2
Cyrill Gurtner <me@cgurtner.ch>
2022-04-07 21:03:52 +0200

    fix TaxReport output even more

--- BankApplication/TaxReport.py
+++ BankApplication/TaxReport.py
@@ -10,6 +10,7 @@ class TaxReport:
     def generate(self):
         print('\n###### Tax-Report ######\n')
         account_list = self.app.get_accounts()
+        total_tax = 0
         for key, acc in account_list.items():
             s = '** ' + acc.__class__.__name__ + ' ' + acc.get_iban() + ' ** '
             if acc.get_currency() == TaxReport.TAX_REPORT_CURRENCY:
@@ -18,7 +19,9 @@ class TaxReport:
                 self.api.set_base_code(acc.get_currency())
                 self.api.set_target_code(TaxReport.TAX_REPORT_CURRENCY)
                 self.api.conversion(acc.get_balance())
+                total_tax += self.api.get_conversion_result()
                 s += self.api.get_formatted_conversion_result() + ' ' + self.api.get_target_code() + ' '
                 s += '(' + acc.get_formatted_balance() + ' ' + acc.get_currency() + ' '
-                s += '[ exchange-rate: ' + str(self.api.get_conversion_rate()) + ' ])'
-            print(s)
+                s += '[exchange-rate: ' + str(self.api.get_conversion_rate()) + '])'
+        s += '\n\n Total TAX: ' + format(total_tax, '.2f')
+        print(s)
