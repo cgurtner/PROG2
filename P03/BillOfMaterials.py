7992a16c2f5ae910e27f271166815cf1a1bc92ec
Cyrill Gurtner <me@cgurtner.ch>
2022-04-08 07:08:03 +0200

    added BillOfMaterials.py

---
+++ P03/BillOfMaterials.py
@@ -0,0 +1,37 @@
+from BillOfMaterialsProvider import BillOfMaterialsProvider
+
+class BillOfMaterials:
+    def __init__(self) -> None:
+        self.api = BillOfMaterialsProvider()
+    
+    def print_bill(self) -> None:
+        df = self.api.get_data()
+        total = df['cost'].sum()
+        s = ''
+
+        for index in df.index:
+            material = df['material'][index]
+            cost = df['cost'][index]
+            s += '{position} | {cost} \n'.format(
+                position=BillOfMaterials.format_position(material), 
+                cost=BillOfMaterials.format_cost(cost)
+            )
+
+        s += '----------------+----------\n'
+        s += '{position} | {total}'.format(
+            position=BillOfMaterials.format_position('SUM'), 
+            total=BillOfMaterials.format_cost(total)
+        )
+        
+        print(s)
+            
+    def format_cost(cost) -> str:
+        return format(cost, '.2f')
+
+    def format_position(p) -> str:
+        return '{:<15}'.format(p)
+
+if __name__ == '__main__':
+    bom = BillOfMaterials()
+    bom.print_bill()
+    
